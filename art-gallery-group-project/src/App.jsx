0ad988191a26347ad808138c3fa76886e2bc5872
Ezekiel <realnyamasege2@gmail.com>
2024-05-07 17:12:22 +0300

    1st step

---
+++ art-gallery-group-project/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+// import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+<h1 className="text-3xl font-bold underline text-blue-500">
+      Art Gallery
+    </h1>
+    </div>
+  )
+}
+
+export default App
+
+
+// artwork component
+// contactform component
+// search component
+// navigation
+//artcollection
+//navbar => collection, about
+// pagenation
